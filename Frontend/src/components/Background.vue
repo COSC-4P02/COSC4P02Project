6ea8b4bfc4fbb2509001a486f93acb9185d0ede5
ICARUSCLOVER <37007493+ICARUSCLOVER@users.noreply.github.com>
2022-03-07 15:43:59 -0500

    background card size

--- Frontend/src/components/Background.vue
+++ Frontend/src/components/Background.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -7,13 +7,25 @@
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="../assets/brock/Brock.jpg" class="d-block w-100" alt="...">
+        <img src="../assets/brock/Brock.jpg" class="d-block w-100 min-vh-60" style="height: 70vh" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
       </div>
       <div class="carousel-item">
-        <img src="../assets/brock/Brocku2.jpeg" class="d-block w-100" alt="...">
+        <img src="../assets/brock/Brocku2.jpeg" class="d-block w-100 min-vh-60" style="height: 70vh" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
       </div>
       <div class="carousel-item">
-        <img src="../assets/canada_game/canada-games2.jpeg" class="d-block w-100" alt="...">
+        <img src="../assets/canada_game/canada-games2.jpeg" class="d-block w-100 min-vh-60" style="height: 70vh" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
