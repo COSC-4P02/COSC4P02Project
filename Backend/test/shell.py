020c13ac159a37661e99adfe9c51b7d4de45ebc4
Krunk Zhou <bit86@live.com>
2022-03-20 20:22:02 -0400

    Add Clean Script

--- Backend/test/shell.py
+++ Backend/test/shell.py
@@ -8,4 +8,27 @@ import os
 import time
 from sys import platform
 
-print("Ready")
+path = ""
+if platform == "linux" or platform == "linux2":
+    path = os.path.dirname(os.path.realpath(__file__)).replace("test","data/nltk")
+elif platform == "darwin":
+    path = os.path.dirname(os.path.realpath(__file__)).replace("test","data/nltk")
+elif platform == "win32":
+    path = os.path.dirname(os.path.realpath(__file__)).replace("test","data\\nltk")
+
+nltk.data.path.append(path)
+
+def nltk_download(name,find):
+    try:
+        print(nltk.data.find(find))
+    except LookupError:
+        nltk.download(name, download_dir = path)
+
+def main():
+	nltk_download('punkt','tokenizers/punkt')
+	nltk_download('wordnet','corpora/wordnet')
+	nltk_download('omw-1.4','corpora/omw-1.4')
+	print("Ready")
+
+if __name__ == "__main__":
+	main()
