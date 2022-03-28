a0df53e1fedee2b7cc1dd3b57ab2c9c682f2b06c
charles11223344 <charl0681@gmail.com>
2022-03-28 13:31:19 -0400

    new upgrade
    Co-Authored-By: lwj-super7 <64762591+lwj-super7@users.noreply.github.com>

--- Crawler/final_exam_timetable/finalexam.py
+++ Crawler/final_exam_timetable/finalexam.py
@@ -5,16 +5,28 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
 }
 
-file = open("ACTG.csv", "w", encoding="utf-8")
+file = open("CHEM.csv", "w", encoding="utf-8")
 
 chromeOptions = webdriver.ChromeOptions()
-chromeOptions.add_argument('headless')
+#chromeOptions.add_argument('headless')
 
 driver = webdriver.Chrome(chrome_options=chromeOptions)
 
-driver.get('https://brocku.ca/guides-and-timetables/timetables/?session=fw&type=ex&level=all&program=ACTG&academicyear=2021&period=April')
+driver.get('https://brocku.ca/guides-and-timetables/timetables/?session=fw&type=ex&level=all&program=ECON&academicyear=2021&period=April')
 
 #driver = driver.find_element_by_tag_name('html')
-table = driver.find_element_by_xpath("//table[contains(@clsss,'course-table') and contains(@class,'exam-listing')]")
+table = driver.find_element_by_xpath("//table[contains(@class,'course-table') and contains(@class,'exam-listing')]")
+
+
+
+
+
+print("Table:")
+
+print(table.text)
+
+
+
+driver.close()
 
 
