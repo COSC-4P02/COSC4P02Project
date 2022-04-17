a56ec68cafc39a82188c9f700facfed732430050
ICARUSCLOVER <37007493+ICARUSCLOVER@users.noreply.github.com>
2022-04-17 15:18:25 -0400

    links

--- Frontend/src/components/Nav.vue
+++ Frontend/src/components/Nav.vue
@@ -15,7 +15,7 @@
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li class="nav-item">
-              <a v-if="!this.GLOBAL.ifAPP" class="nav-link active" aria-current="page" :href="botInfo.OfficialWeb">Official Web</a>
+              <div v-if="!this.GLOBAL.ifAPP" class="nav-link active" aria-current="page" >Web Setting</div>
             </li>
             <li><hr class="dropdown-divider"></li>
             <li class="nav-item dropdown">
@@ -41,20 +41,23 @@
                 <li><a class="dropdown-item" @click="fontChange('2')">Large</a></li>
               </ul>
             </li>
+            <li class="nav-item">
+              <div v-if="!this.GLOBAL.ifAPP" class="nav-link active" aria-current="page" >Useful Links</div>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li class="nav-item dropdown" v-for="(items) in links.list" :key="items" >
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{items.type}}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+                <li v-for="(item) in items.links" :key="item"><a class="dropdown-item" :href="item.link">{{item.label}}</a></li>
+              </ul>
+            </li>
           </ul>
           <form v-if="!this.GLOBAL.ifAPP" class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Type what you want" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
           </form>
-          <li><hr class="dropdown-divider"></li>
-          <li class="nav-item dropdown" v-for="(items,index) in links" :key="items" >
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              {{items}}
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-              <li v-for="(items) in links[index]" :key="items"><a class="dropdown-item" :href="items.link"></a></li>
-            </ul>
-          </li>
         </div>
       </div>
     </div>
@@ -88,8 +91,7 @@ export default {
         navTitle: '',
         OfficialWeb: ''
       },
-      links: {
-      }
+      links: ''
     }
   },
   methods: {
@@ -104,12 +106,14 @@ export default {
           this.botInfo.IconAlt = 'Niagara 2022 Canada Games logo'
           this.botInfo.navTitle = 'Niagara 2022 Games Bot'
           this.botInfo.OfficialWeb = 'https://niagara2022games.ca/'
+          this.links = require('./BrockuLink.json')
         } else if (newValue === 'brock') {
           this.botInfo.botTitle = 'Brock University'
           this.botInfo.botIcon = require('../assets/brock/brocku-logo-rgb.png')
           this.botInfo.IconAlt = 'Brock University logo'
           this.botInfo.navTitle = 'Brock Bot'
           this.botInfo.OfficialWeb = 'https://brocku.ca/'
+          this.links = require('./CgamesLink.json')
         }
       }
     }
