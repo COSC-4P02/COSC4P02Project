1d7c7d51fd3c8855d97e6e3b2a20526f264c95f9
ICARUSCLOVER <37007493+ICARUSCLOVER@users.noreply.github.com>
2022-04-18 16:48:41 -0400

    unit test

--- Frontend/tests/unit/example.spec.js
+++ Frontend/tests/unit/example.spec.js
@@ -1,12 +1,81 @@
 import { shallowMount } from '@vue/test-utils'
 import Background from '@/components/background/Background'
+import Nav from '@/components/Nav'
+import App from '@/App.vue'
+import Vue from 'vue'
+import _global_ from '@/global'
+Vue.prototype.GLOBAL = _global_
+
+const bk = (values = {}) => {
+  return shallowMount(Background, {
+    data () {
+      return {
+        msg: {
+          items: [{
+            title: 'brock',
+            description: '',
+            image: '../assets/brock/Brock.jpg',
+            link: ''
+          }]
+        }
+      }
+    },
+    propsData: { version: 'brock' }
+  })
+}
 
 describe('Background.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(Background, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+  it('renders props .version when passed', () => {
+    const wrapper = bk
+    expect(wrapper.find('.items.title').text()).toEqual('brock')
+  })
+})
+
+const nav = (values = {}) => {
+  return shallowMount(Nav, {
+    data () {
+      return {
+        msg: {
+          items: [{
+            title: 'brock',
+            description: '',
+            image: '../assets/brock/Brock.jpg',
+            link: ''
+          }]
+        }
+      }
+    },
+    propsData: { version: 'brock' }
+  })
+}
+
+describe('Nav.vue', () => {
+  it('renders props .version when passed', () => {
+    const wrapper = nav
+    expect(wrapper.find('.items.title').text()).toEqual('brock')
+  })
+})
+
+const app = (values = {}) => {
+  return shallowMount(App, {
+    data () {
+      return {
+        msg: {
+          items: [{
+            title: 'brock',
+            description: '',
+            image: '../assets/brock/Brock.jpg',
+            link: ''
+          }]
+        }
+      }
+    }
+  })
+}
+
+describe('App.vue', () => {
+  it('renders props .version when passed', () => {
+    const wrapper = app
+    expect(wrapper.find('.items.title').text()).toEqual('brock')
   })
 })
