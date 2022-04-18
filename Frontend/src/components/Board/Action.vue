0c3ea415654d22739e2b6922630fae2cb1a5bfcf
ICARUSCLOVER <37007493+ICARUSCLOVER@users.noreply.github.com>
2022-04-18 01:45:14 -0400

    empty input

--- Frontend/src/components/Board/Action.vue
+++ Frontend/src/components/Board/Action.vue
@@ -3,6 +3,17 @@
   :class="actionClass"
 )
   .qkb-board-action__wrapper
+    .qkb-board-action__extra
+      slot(name="actions")
+      button.qkb-action-item( v-if="messageText", @click="empty")
+        slot(name="emptyButton")
+          svg(xmlns="http://www.w3.org/2000/svg",
+            width="16",
+            height="16",
+            fill="currentColor",
+            class="bi bi-x-circle-fill",
+            viewBox="0 0 16 16")
+              path(d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z")
     .qkb-board-action__msg-box
       input.qkb-board-action__input(
         type="text",
@@ -80,6 +91,11 @@ export default {
     sendMessage () {
       if (this.messageText) {
         this.$emit('msg-send', { text: this.messageText })
+        this.messageText = null
+      }
+    },
+    empty () {
+      if (this.messageText) {
         this.messageText = null
       }
     },
